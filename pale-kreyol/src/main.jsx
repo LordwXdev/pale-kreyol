a53740f2e78663eb0b5893c2a7af414bf2951622
Lordwish Sylvaince <lordwishsylvaince1236@gmail.com>
2025-12-07 05:21:22 +0800

    ..

--- pale-kreyol/src/main.jsx
+++ pale-kreyol/src/main.jsx
@@ -1,7 +1,14 @@
-import { AuthProvider } from "./context/AuthContext";
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 );
